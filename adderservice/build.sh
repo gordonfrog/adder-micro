6e6c1c8d3530293b863770c682b2c31a210ae3ea
william gordon <williamgordon@williams-MBP.fios-router.home>
2019-07-14 18:06:54 -0500

    jenkins

--- adderservice/build.sh
+++ adderservice/build.sh
@@ -5,15 +5,15 @@ GITSHA=$(git rev-parse --short HEAD)
 
 case "$1" in
   container)
-    sudo -u jenkins docker build -t adderservice:$GITSHA .
-    sudo -u jenkins docker tag adderservice:$GITSHA gordonfrog/adderservice:$GITSHA 
-    sudo -i -u jenkins docker push gordonfrog/adderservice:$GITSHA 
+    sudo -u williamgordon docker build -t adderservice:$GITSHA .
+    sudo -u williamgordon docker tag adderservice:$GITSHA gordonfrog/adderservice:$GITSHA 
+    sudo -i -u williamgordon docker push gordonfrog/adderservice:$GITSHA 
   ;;
   deploy)
     sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/  < ../deployment/service-template.yml > svc.yml
     sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/ -e s/_IMAGE_/gordonfrog\\/adderservice:$GITSHA/ < ../deployment/deployment-template.yml > dep.yml
-    sudo -i -u jenkins kubectl apply -f $(pwd)/svc.yml
-    sudo -i -u jenkins kubectl apply -f $(pwd)/dep.yml
+    sudo -i -u williamgordon kubectl apply -f $(pwd)/svc.yml
+    sudo -i -u williamgordon kubectl apply -f $(pwd)/dep.yml
   ;;
   *)
     echo 'invalid build command'
